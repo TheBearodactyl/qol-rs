361f75ac0350ef338561cbfed86e8bbbcd46d7d4
TheBearodactyl <dabewodactowluwu@gmail.com>
2023-12-15 12:35:14 -0600

    owo

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,4 @@
-#[cfg(feature = "funcs")]
 pub mod functions; // Functions
-#[cfg(feature = "macros")]
 pub mod macros; // Macros
-#[cfg(feature = "sae")]
 pub mod structs_and_enums; // Structs and Enums
-#[cfg(feature = "type_aliases")]
-pub mod type_aliases;
-#[cfg(feature = "generics")]
-pub mod generics;
+pub mod generics; // Generics (Function Templates)
